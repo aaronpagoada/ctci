0e5b7c8653ead482a1668435da5ffef4ad09b145
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-01-01 15:14:21 -0500

    Finished big o chapter

--- chapters/bigO.js
+++ chapters/bigO.js
@@ -75,3 +75,67 @@ function sqrt(n){
     return -1
 }
 // Runtime is O(sqrt(n)), guessed incorrectly that it was O(log n)
+
+// 7
+// If a binary search tree is unbalanced, how long might it take (worst case) to find an element in it?
+// Runtime is O(n), where n is the number of nodes in the tree
+
+// 8
+// You are looking for a specific value in a binary tree, but the tree is not a binary search tree. What is the time complexity of this?
+// Runtime is O(n)
+
+// 9
+function copyArray(arr){
+    let copy = new Array(0)
+    for(let value of arr){
+        copy = appendToNew(copy, value)
+    }
+    
+    return copy
+}
+
+function appendToNew(arr, value){
+    let bigger = new Array(arr.length + 1)
+    for(let i = 0; i < arr.length; i++){
+        bigger[i] = arr[i]
+    }
+
+    bigger[bigger.length - 1] = value
+    return bigger
+}
+// Runtime is O(n^2)
+
+// 10
+function sumDigits(n){
+    let sum = 0
+    while(n > 0){
+        sum += n % 10
+        n /= 10
+    }
+    return sum
+}
+// Runtime is O(n), where n is the number of digits in n
+// reasoning was correct but not answer, runtime is O(log n) because a number with d digits can have a value up to 10^d
+// if n = 10^d, then d = log n
+
+// 11
+// The following code prints all strings of length k where the characters are in sorted order.
+// It does this by generating all strings of length k and checking if each is sorted.
+// Runtime is O(k^2), guessed incorrectly, correct runtime is O(kc^k) where k is length of string and c is number of chars in alphabet
+
+// 12
+function intersection(a, b){
+    mergeSort(b)
+    let intersect = 0
+
+    for(let value of a){
+        if(binarySearch(b, value) >= 0){
+            intersect++
+        }
+    }
+
+    return intersect
+}
+// Runtime is O(b log b + a log b)
+
+// 9/12 == 75%
