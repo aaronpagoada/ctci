c39878ddb0474011e592a596d871fa9f959ab106
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-01-04 15:56:14 -0500

    Added URLify runtime

--- chapters/arraysStrings/URLify.js
+++ chapters/arraysStrings/URLify.js
@@ -2,8 +2,8 @@
 // Write a methiod to replace all spaces in a string with "%20".
 
 function URLify(str){
-  return str.split(" ").join("%20")
-}
+  return str.split(" ").join("%20") // O(n) + O(n)
+} // O(n)
 
 console.log(URLify("my cat is cool")) // my%20cat%20is%20cool
 console.log(URLify("catsarecool")) // catsarecool
