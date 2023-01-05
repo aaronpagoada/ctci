e7d577680bb43333c915de18c3e7fb4cf7c7192c
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-01-05 16:48:15 -0500

    added palindromePermutation runtime

--- chapters/arraysStrings/palindromePermutation.js
+++ chapters/arraysStrings/palindromePermutation.js
@@ -5,9 +5,9 @@
 // if length is odd, all letters but one need a pair
 
 function palindromePermutation(str){
-  let string = str.split(" ").join("")
+  let string = str.split(" ").join("") // O(n) + O(n)
   let map = {}
-  for(let i = 0; i < string.length; i++){
+  for(let i = 0; i < string.length; i++){ // O(n)
     if(map[string[i]] == null){
       map[string[i]] = 1
     } else {
@@ -15,7 +15,7 @@ function palindromePermutation(str){
     }
   }
 
-  let freqs = Object.values(map).map(value => {
+  let freqs = Object.values(map).map(value => { // O(n)
     return value % 2
   })
 
@@ -25,12 +25,12 @@ function palindromePermutation(str){
     }
     return true
   } else {
-    if(freqs.filter(value => value == 1).length == 1){
+    if(freqs.filter(value => value == 1).length == 1){ // O(n)
       return true
     }
     return false
   }
-}
+} // O(4n) == O(n)
 
 console.log(palindromePermutation("aaab")) // false
 console.log(palindromePermutation("tact coa")) // true
