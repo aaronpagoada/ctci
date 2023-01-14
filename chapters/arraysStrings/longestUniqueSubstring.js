a509d4db6fb8f9ca720995547636dcdba46c9c10
AP <74792335+aaronpagoada@users.noreply.github.com>
2023-01-14 15:19:43 -0500

    Added longestUniqueSubstring

---
+++ chapters/arraysStrings/longestUniqueSubstring.js
@@ -0,0 +1,31 @@
+// Longest Unique Substring
+// Given a string, find the length of the longest substring without repeating characters.
+
+function longestUniqueSubstring(s){
+  if(s.length == 0){
+    return 0
+  }
+
+if(s.length == 1){
+    return 1
+  }
+
+  let maxLength = 0
+  let longestString = []
+
+  for(let i = 0; i < s.length; i++){
+    let currentCharPosition = longestString.indexOf(s[i])
+
+    if(currentCharPosition != -1){
+      longestString.splice(0, currentCharPosition + 1)
+    }
+
+    longestString.push(s[i])
+    maxLength = Math.max(longestString.length, maxLength)
+  }
+
+  return maxLength
+}
+
+console.log(longestUniqueSubstring("abccabcbb")) // 3
+console.log(longestUniqueSubstring("fffffffa")) // 2
